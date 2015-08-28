8b445478e2ed611830d420e6a217bd003fcf9bdd
iwasaki.kohei <iwasaki.kohei@donuts.ne.jp>
2015-08-28 12:37:36 +0900

    Add health check routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   root 'articles#index'
+  get '/health' => 'application#health'
 
   devise_for :users, controllers: {
     omniauth_callbacks: "users/omniauth_callbacks"
