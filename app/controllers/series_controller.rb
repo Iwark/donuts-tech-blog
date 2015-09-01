a4f49cc27760ffafa11f923ef90460be882786dd
iwasaki.kohei <iwasaki.kohei@donuts.ne.jp>
2015-09-01 15:21:32 +0900

    Add index

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -5,6 +5,5 @@ class SeriesController < ApplicationController
   def show(id, page=1)
     @series          = Series.find(id)
     @articles        = @series.articles.approved_by_page(page)
-    @resent_articles = @series.articles.approved_by_page(1, 5)
   end
 end
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,6 +5,5 @@ class TagsController < ApplicationController
   def show(id, page=1)
     @tag             = Tag.find(id)
     @articles        = @tag.articles.approved_by_page(page)
-    @resent_articles = @tag.articles.approved_by_page(1, 5)
   end
 end
